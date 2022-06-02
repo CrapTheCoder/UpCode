fddc16db112f8b2c45e5e31a34bb594531f8bd06
Parth Santosh Kabra <84372000+parth-kabra@users.noreply.github.com>
2022-06-02 15:28:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,14 +10,12 @@ from multiprocessing import Process
 
 def upload_solution(website, solution, repo):
     try:
-        if 'c++' in solution['language'].lower():
-            extension = 'cpp'
-        elif 'py' in solution['language'].lower():
-            extension = 'py'
-        elif 'java' in solution['language'].lower():
-            extension = 'java'
+        s = solution["language"]
+        ind = s.rfind(".")
+        if ind == -1:
+            extension = "txt"
         else:
-            extension = 'txt'
+            extension = s[ind + 1:]
 
         upload_to_github(repo, f'{website}/{solution["language"]}/{solution["problem_code"]}/{solution["solution_id"]}.{extension}', solution['solution'])
         return True
