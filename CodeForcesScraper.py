548b4175f14f22cbec1efdedda14bfa166ff77b6
Srivaths P <41336229+CrapTheCoder@users.noreply.github.com>
2022-12-25 01:31:24 +0530

    Update CodeForcesScraper.py

--- CodeForcesScraper.py
+++ CodeForcesScraper.py
@@ -59,7 +59,7 @@ def get_solutions(username, all_info=None):
     options = Options()
     options.add_argument('headless')
     options.add_argument('window-size=1920x1080')
-    options.add_argument("disable-gpu")
+    options.add_argument('disable-gpu')
 
     driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
     driver.get(f'https://codeforces.com/submissions/{username}')
